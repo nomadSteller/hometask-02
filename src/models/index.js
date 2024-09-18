5e0c6d3266a918245232158433d8227e24650db1
MayankAgrawal94 <mayankagrawal4949@gmail.com>
2024-09-18 13:48:33 +0530

    Model export index file updated
    sequelize db connection added here in order to export from Model

--- src/models/index.js
+++ src/models/index.js
@@ -1,15 +1,22 @@
+const sequelize = require('../config/db');
 const Profile = require('./profile.model');
 const Contract = require('./contract.model');
 const Job = require('./job.model');
 
 Profile.hasMany(Contract, {as :'Contractor',foreignKey:'ContractorId'})
 Contract.belongsTo(Profile, {as: 'Contractor'})
+
 Profile.hasMany(Contract, {as : 'Client', foreignKey:'ClientId'})
 Contract.belongsTo(Profile, {as: 'Client'})
+
 Contract.hasMany(Job)
 Job.belongsTo(Contract)
 
+// Sync the models with the database
+sequelize.sync()
+
 module.exports = {
+    sequelize,
     Profile,
     Contract,
     Job
