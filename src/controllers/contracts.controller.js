062dc2fd214841aef947a2ac9b41aa79c5b712b4
MayankAgrawal94 <mayankagrawal4949@gmail.com>
2024-09-18 12:05:35 +0530

    Fixed functionality for route '/contracts/:id'
    contracts controller function 'getContractById' fixed.

--- src/controllers/contracts.controller.js
+++ src/controllers/contracts.controller.js
@@ -1,15 +1,23 @@
 const { Contract } = require('../models');
 const { Op } = require('sequelize');
 
-/**
- * FIX ME!
- * @returns contract by id
- */
 const getContractById = async (req, res) => {
-  const {id} = req.params
-  const contract = await Contract.findOne({where: {id}})
-  if(!contract) return res.status(404).end()
-  res.json(contract)
+  const { id } = req.params;
+  const profileId = req.profile.id;
+
+  const contract = await Contract.findOne({ 
+    where: {
+      id,
+      [Op.or]: [
+        { ClientId: profileId },
+        { ContractorId: profileId }
+      ]
+    }
+  });
+
+  if (!contract) return res.status(404).end();
+
+  res.json(contract);
 };
 
 module.exports = { getContractById };
