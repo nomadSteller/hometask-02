2868eae2a0d4311e6c03197602581ffc8ce33788
MayankAgrawal94 <mayankagrawal4949@gmail.com>
2024-09-18 12:43:21 +0530

    New route added '/jobs/:job_id/pay'
    Pay for a job, a client can only pay if his balance >= the amount to pay. The amount should be moved from the client's balance to the contractor balance.

--- src/controllers/jobs.controller.js
+++ src/controllers/jobs.controller.js
@@ -22,4 +22,41 @@ const getUnpaidJobs = async (req, res) => {
   res.json(jobs);
 }
 
-module.exports = { getUnpaidJobs }
+const payForJob = async (req, res) => {
+  const { job_id } = req.params;
+  const profileId = req.profile.id;
+
+  try {
+    // Start a transaction
+    await sequelize.transaction(async (transaction) => {
+      // Inside this function, Sequelize operations are part of the transaction
+
+      const job = await Job.findOne({ where: { id: job_id }, include: Contract }, { transaction });
+      if (!job) return res.status(404).end();
+
+      const client = await Profile.findOne({ where: { id: job.Contract.ClientId } }, { transaction });
+      const contractor = await Profile.findOne({ where: { id: job.Contract.ContractorId } }, { transaction });
+
+      if (client.balance < job.price) return res.status(400).json({ error: 'Insufficient balance' });
+
+      // Update balances
+      client.balance -= job.price;
+      contractor.balance += job.price;
+      contractor.status = 'terminated'  // assuming the job got completed after that only client is making payment.
+      job.paid = true;
+
+      // Save updated entities within the transaction
+      await client.save({ transaction });
+      await contractor.save({ transaction });
+      await job.save({ transaction });
+    });
+    // If no errors were thrown inside the transaction block, Sequelize commits the transaction automatically
+
+    res.json({ message: 'Payment successful' });
+  } catch (error) {
+    console.error('Transaction failed:', error);
+    res.status(500).json({ error: 'Transaction failed' });
+  }
+}
+
+module.exports = { getUnpaidJobs, payForJob }
--- src/routes/jobs.route.js
+++ src/routes/jobs.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { getProfile } = require('../middlewares/getProfile');
-const { getUnpaidJobs } = require('../controllers/jobs.controller');
+const { getUnpaidJobs, payForJob } = require('../controllers/jobs.controller');
 const router = express.Router();
 
 router.get('/unpaid', getProfile, getUnpaidJobs);
+router.post('/:job_id/pay', getProfile, payForJob);
 
 module.exports = router;
